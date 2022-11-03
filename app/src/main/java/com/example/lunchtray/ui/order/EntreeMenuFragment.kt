3e3989d1092c206e59fb1f040dc662aeeeaef824
Luigi <luigiiorio30@gmail.com>
2022-11-03 10:34:04 +0100

    Completed TODO: added ui.order fragment navigation and layout fragment data

--- app/src/main/java/com/example/lunchtray/ui/order/EntreeMenuFragment.kt
+++ app/src/main/java/com/example/lunchtray/ui/order/EntreeMenuFragment.kt
@@ -22,6 +22,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.lunchtray.R
 import com.example.lunchtray.databinding.FragmentEntreeMenuBinding
 import com.example.lunchtray.model.OrderViewModel
@@ -60,7 +61,7 @@ class EntreeMenuFragment : Fragment() {
         binding.apply {
             lifecycleOwner = viewLifecycleOwner
             viewModel = sharedViewModel
-            // TODO: initialize the EntreeMenuFragment variables
+            entreeMenuFragment = this@EntreeMenuFragment
         }
     }
 
@@ -68,16 +69,15 @@ class EntreeMenuFragment : Fragment() {
      * Navigate to the side menu fragment.
      */
     fun goToNextScreen() {
-        // TODO: Navigate to the SideMenuFragment
-      //  findNavController().navigate(R.id.)
+        findNavController().navigate(R.id.action_entreeMenuFragment_to_sideMenuFragment)
     }
 
     /**
      * Cancel the order and start over.
      */
     fun cancelOrder() {
-        // TODO: Reset order in view model
-        // TODO: Navigate back to the [StartFragment] to start over
+        sharedViewModel.resetOrder()
+        findNavController().navigate(R.id.action_entreeMenuFragment_to_startOrderFragment)
     }
 
     /**
